3eda365b42b9d914ff5da4ae697ddbc59ba14cd5
hudson-aptplay <hudson@aptplay.com>
2025-01-30 18:03:31 +0900

    first commit

---
+++ src/main/java/com/medalist/MedalistApplication.java
@@ -0,0 +1,13 @@
+package com.medalist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedalistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedalistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medalist/MedalistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medalist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedalistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
