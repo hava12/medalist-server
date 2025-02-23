80ca4f61de6abcbeb2e3628bf04dd3160c2df49b
hava12 <hava12@naver.com>
2025-02-23 14:09:34 +0900

    terraform 테스트

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -8,26 +8,26 @@ version = "0.0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    maven {
-        name = "GitHubPackages"
-        url = uri(project.findProperty("url") as String)
-        credentials {
-            username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-            password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
-        }
-    }
+//    maven {
+//        name = "GitHubPackages"
+//        url = uri(project.findProperty("url") as String)
+//        credentials {
+//            username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+//            password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+//        }
+//    }
 }
 
 dependencies {
-    implementation("com.hava12.custom-checkstyle:custom-checkstyle:1.0.0-SNAPSHOT")
-    checkstyle("com.hava12.custom-checkstyle:custom-checkstyle:1.0.0-SNAPSHOT")
+//    implementation("com.hava12.custom-checkstyle:custom-checkstyle:1.0.0-SNAPSHOT")
+//    checkstyle("com.hava12.custom-checkstyle:custom-checkstyle:1.0.0-SNAPSHOT")
 }
 
-checkstyle {
-    toolVersion = "10.21.1"  // Checkstyle 버전
-    configFile = file("config/checkstyle/checkstyle-custom.xml")
-    isShowViolations = true
-}
+//checkstyle {
+//    toolVersion = "10.21.1"  // Checkstyle 버전
+//    configFile = file("config/checkstyle/checkstyle-custom.xml")
+//    isShowViolations = true
+//}
 
 tasks.register<Checkstyle>("checkCommon") {
     configFile = file("config/checkstyle/checkstyle-common.xml")
